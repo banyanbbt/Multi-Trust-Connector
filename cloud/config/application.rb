c17a6f2581a276d0a7ddf525093f792cb586c631
henryli <manageyp@gmail.com>
2018-05-16 10:42:11 +0800

    [Change] application time zone, encoding configs

--- cloud/config/application.rb
+++ cloud/config/application.rb
@@ -14,5 +14,11 @@ module Cloud
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone='Beijing'
+    config.active_record.default_timezone = :local
+    config.encoding = "utf-8"
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+
   end
 end
