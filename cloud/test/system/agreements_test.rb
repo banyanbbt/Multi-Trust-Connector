ba5e822709a581fa97d3c5381efd28e4a5e7b931
henryli <manageyp@gmail.com>
2018-05-17 16:06:30 +0800

    [New] add agreement controller, views and tests

---
+++ cloud/test/system/agreements_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class AgreementsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit agreements_url
+  #
+  #   assert_selector "h1", text: "Agreement"
+  # end
+end
