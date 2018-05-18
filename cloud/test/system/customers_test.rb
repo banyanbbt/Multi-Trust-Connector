1af7e5bcdfe8bcf5bee9fdf430c3272199afb240
henryli <henryli@banyanbbt.org>
2018-05-18 15:51:41 +0800

    [New] customer controller, views and tests

---
+++ cloud/test/system/customers_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CustomersTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit customers_url
+  #
+  #   assert_selector "h1", text: "Customer"
+  # end
+end
