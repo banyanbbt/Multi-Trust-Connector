e7a25033c7f93fb852bb4445428ea7986bc4f6b8
henryli <manageyp@gmail.com>
2018-05-21 13:43:51 +0800

    [New] customer_agreement controller, views and tests

---
+++ cloud/app/helpers/customer_agreements_helper.rb
@@ -0,0 +1,2 @@
+module CustomerAgreementsHelper
+end
---
+++ cloud/test/system/customer_agreements_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CustomerAgreementsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit customer_agreements_url
+  #
+  #   assert_selector "h1", text: "CustomerAgreement"
+  # end
+end
