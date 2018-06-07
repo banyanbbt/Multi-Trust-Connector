7fc11858cec270f52be6e0ae4867f2d2b49996ae
henryli <manageyp@gmail.com>
2018-06-07 16:39:57 +0800

    [New] agreement_filling model and migration

---
+++ cloud/app/models/agreement_filling.rb
@@ -0,0 +1,2 @@
+class AgreementFilling < ApplicationRecord
+end
---
+++ cloud/test/models/agreement_filling_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AgreementFillingTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
